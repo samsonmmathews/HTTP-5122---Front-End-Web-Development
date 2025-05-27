a8679bfc904f7ad080853e8aa70f50dabd359115
Samson Mathews <samsonmmathews@gmail.com>
2025-05-27 11:25:48 -0400

    Week 4 lab work

---
+++ Week 4/Lab/lab-4-2.js
@@ -0,0 +1,30 @@
+﻿//#### LAB 4 - FUNCTIONS ####
+//PART 2:  AN AVERAGE FUNCTION
+
+
+//################## CREATE YOUR AVERAGE FUNCTION
+//This function takes five numbers and returns their average to one decimal place.
+function averageNum(num1, num2, num3, num4, num5)
+{
+    var average = ((num1 + num2 + num3 + num4 + num5)/5).toFixed(1);
+    return average;
+}
+
+var avg = averageNum(5, 10, 15, 20, 25);
+alert("Average value is " + avg);
+
+
+//################## LOGIC THAT OUTPUTS MESSAGES BASED ON FUNCTION RESULTS
+
+var http5122 = 85, http5110 = 70, http5121 = 90, http5125 = 75, http5126 = 80;
+
+var avgMarks = averageNum(http5110, http5121, http5122, http5125, http5126);
+
+if(avgMarks > 70)
+{
+    alert("Success! Your average is " + avgMarks + ", which is greater than 70");
+}
+else
+{
+    alert("Review required! Your average is " + avgMarks + ", which is less than 70");
+}
---
+++ Week 4/Lab/lab-4-3.js
@@ -0,0 +1,42 @@
+﻿//#### LAB 4 - FUNCTIONS ####
+//PART 3:  WALKING THE DOG 
+
+
+//################## CREATE YOUR checkTemp FUNCTION
+//This function will check the current temperature and if it is less than 30, it will display false 
+//and it will display true if it is otherwise
+
+//It expects to receive the current temperature (currentTemp) that was entered by the user as the argument. The data type is a number
+
+//It will return false (boolean values) if the value passed as argument is less than 30 and true if it is otherwise
+function checkTemp(currentTemp)
+{
+    if(currentTemp > 30)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
+
+//################## LOGIC THAT OUTPUTS MESSAGES BASED ON FUNCTION RESULTS
+
+var temp = prompt("What is the current temperature?");
+
+var dogWalkTemp = checkTemp(temp);
+
+if(temp < -10)  // If the temp is less than -10, it will straight up display the message and won't check for the value of the function
+{
+    alert("Yikes! This is no weather for dog walking!");
+}
+else if(dogWalkTemp == false)   // Check for the return value of the function and decides the display message
+{
+    alert("Yikes! Too hot for dog walking!");
+}
+else
+{
+    alert("You’re good, have a nice walk!");
+}
