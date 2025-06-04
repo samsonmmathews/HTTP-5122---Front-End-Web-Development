9f0d5680ed03b1a46a891f265a5825085de0d560
Samson Mathews <samsonmmathews@gmail.com>
2025-06-04 09:21:46 -0400

    Started lab work

---
+++ Week 5/Lab/lab-5-1.js
@@ -0,0 +1,8 @@
+//#### LAB 5 - FUNCTIONS & OBJECTS ####
+//PART 1:  I OBJECT!
+var meObject = {
+    firstName: "Samson",
+    lastName: "Mathew",
+    age: 27,
+    yearOfBirth: 1997
+};
---
+++ Week 5/Lab/lab-5-2.js
@@ -0,0 +1,10 @@
+//#### LAB 5 - FUNCTIONS & OBJECTS ####
+//PART 2:  MAKE THE BANK
+//1. Create the object structure first.
+//2. Add the required properties to your object.
+//3. Add your first method and test it. Remember, the methods will change the properties of the object.
+//4. Add your second method and test it.
+//5. Create the required output to complete steps 6-10 of the lab.
+//6. Once everything is working, tackle the Stretch Goal!
+
+
