5d99a8ba3a0c32fe59dc1f7f87ec44cfcbcfd93b
Samson Mathews <samsonmmathews@gmail.com>
2025-06-05 10:44:12 -0400

    Week 5 lab work

--- Week 5/Lab/lab-5-1.js
+++ Week 5/Lab/lab-5-1.js
@@ -4,5 +4,18 @@ var meObject = {
     firstName: "Samson",
     lastName: "Mathew",
     age: 27,
-    yearOfBirth: 1997
+    yearOfBirth: 1997,
+    name : function() {
+        alert("My first name is " + meObject.firstName + " and my last name is " + meObject.lastName);
+    }
 };
+
+// Use the console to output one of those properties
+console.log("First name: " + meObject.firstName);
+
+// Create a popup that will output two of those properties concatenated together
+// alert("My first name is " + meObject.firstName + " and my last name is " + meObject.lastName);
+
+// Calling the method inside the object
+
+meObject.name();
--- Week 5/Lab/lab-5-2.js
+++ Week 5/Lab/lab-5-2.js
@@ -1,10 +1,46 @@
 //#### LAB 5 - FUNCTIONS & OBJECTS ####
 //PART 2:  MAKE THE BANK
 //1. Create the object structure first.
+
+var bankCustomer = {
+    lastName: "Smith",
+    branchNumber: 1000,
+    accountBalance: 500.25,
+    interestRate: 1.03,
+    multipleAccounts: false,
+    makeDeposit: function(amount) {
+        this.accountBalance += amount;
+        console.log("Thank you, your current balance is now $" + this.accountBalance.toFixed(2));
+    },
+    makeWithdrawal: function(amount) {
+        this.accountBalance -= amount;
+        console.log("Thank you, your current balance is now $" + this.accountBalance.toFixed(2));
+    },
+    addInterest: function() {
+        var tempInterest = this.interestRate;
+
+        if(this.multipleAccounts == true)
+        {
+            tempInterest += 0.005;
+        }
+
+        this.accountBalance *= tempInterest;
+        console.log("Thank you, your current balance is now $" + this.accountBalance.toFixed(2));
+    }
+};
 //2. Add the required properties to your object.
 //3. Add your first method and test it. Remember, the methods will change the properties of the object.
 //4. Add your second method and test it.
 //5. Create the required output to complete steps 6-10 of the lab.
+
+console.log("Starting balance: $" + bankCustomer.accountBalance);
+
+bankCustomer.makeDeposit(200);  // Deposit $200
+
+bankCustomer.addInterest(); // Adds interest after deposit call
+
+bankCustomer.makeWithdrawal(75);    // Withdraw $75
+
+bankCustomer.addInterest(); // Adds interest after withdrawal call
+
 //6. Once everything is working, tackle the Stretch Goal!
-
-
