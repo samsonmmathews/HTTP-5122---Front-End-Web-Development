8732e82353a3c64816380dac6c4a0af9cf14d950
Samson Mathews <samsonmmathews@gmail.com>
2025-07-29 17:00:42 -0400

    Finished lab work

--- Week 12/Lab/lab10-1-jquery.js
+++ Week 12/Lab/lab10-1-jquery.js
@@ -4,29 +4,32 @@
 $(document).ready( function() {
 	$('.contentBox').hide();
 
-	$('.panelContainer').click(function () {
-        console.log('panelContainer is clicked');
-        var myList = $('.contentBox');
-        // myList.slideDown(1000); // opposite is slideUp()
-        myList.slideToggle(3000);
-    });
-
-
-
-
-
 	//Inside of here is your jQuery/JavaScript
 
 
 	//ADD CLICK EVENT TO <h2>
 	
-	
-	
+	$('.panelContainer h2').click(function () {
+        console.log('panelContainer is clicked');
+
+        // For opening the clicked article
+        $(this).next().slideToggle(3000);
+
+        // Selects all the contentBox items that are not selected to close in 3 seconds
+        $('.contentBox').not($(this).next()).slideUp(3000);
+    });
 	
 	
 	
 	//CHANGE <p> BACKGROUND ON HOVER
-
+    $('.contentBox').hover( function() {
+        console.log('contentBox - hover');
+        $(this).addClass('textHovered');
+    },
+    function() {
+        console.log('contentBox - hover out');
+        $(this).removeClass('textHovered');
+    });
 });
 
 
--- Week 12/Lab/lab10-2-jquery.js
+++ Week 12/Lab/lab10-2-jquery.js
@@ -1,18 +1,24 @@
 //LAB 10 - 2 INVENTORY PAGE
-
-
-
-
-
-
-
+$(document).ready( function() {
+	$('.desc').hide();
 
 	//ADD MOUSEOVER/MOUSEOUT FUNCTIONS FOR <tr>
-
-
-
-
-
-
+	$('#tblInventory tbody tr').hover( function() {
+        console.log('tblInventory - hover');
+        $(this).addClass('selected');
+    },
+    function() {
+        console.log('contentBox - hover');
+        $(this).removeClass('selected');
+    });
 
 	//ADD CLICK EVENT TO <tr>
+
+	$('#tblInventory tbody tr').click(function () {
+
+		$('.desc').hide();
+		$(this).children('.desc').show();
+
+    });
+
+});
