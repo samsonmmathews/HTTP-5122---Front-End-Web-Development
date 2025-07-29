393961a103fc34ae7676e55d9016bf7d730b474d
Samson Mathews <samsonmmathews@gmail.com>
2025-07-29 12:41:49 -0400

    Started lab work

---
+++ Week 12/Lab/lab10-1-jquery.js
@@ -0,0 +1,34 @@
+//LAB 10 - 1 FAQ PAGE
+
+//Listen for window load the jQuery way
+$(document).ready( function() {
+	$('.contentBox').hide();
+
+	$('.panelContainer').click(function () {
+        console.log('panelContainer is clicked');
+        var myList = $('.contentBox');
+        // myList.slideDown(1000); // opposite is slideUp()
+        myList.slideToggle(3000);
+    });
+
+
+
+
+
+	//Inside of here is your jQuery/JavaScript
+
+
+	//ADD CLICK EVENT TO <h2>
+	
+	
+	
+	
+	
+	
+	//CHANGE <p> BACKGROUND ON HOVER
+
+});
+
+
+
+
---
+++ Week 12/Lab/lab10-2-jquery.js
@@ -0,0 +1,18 @@
+//LAB 10 - 2 INVENTORY PAGE
+
+
+
+
+
+
+
+
+	//ADD MOUSEOVER/MOUSEOUT FUNCTIONS FOR <tr>
+
+
+
+
+
+
+
+	//ADD CLICK EVENT TO <tr>
