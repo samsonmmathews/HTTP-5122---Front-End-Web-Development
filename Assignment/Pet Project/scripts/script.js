c76a1934f2e1b6ee11d2ef85e0dae691e35d88c1
Samson Mathews <samsonmmathews@gmail.com>
2025-08-03 18:40:15 -0400

    Added movies

--- Assignment/Pet Project/scripts/script.js
+++ Assignment/Pet Project/scripts/script.js
@@ -7,12 +7,66 @@ window.onload = function() {
     var resetBtn = document.getElementById("reset");
 
     const movies = {
-        happy: ["Movie 1"],
-        sad: ["Movie 2"],
-        excited: ["Movie 3"],
-        relaxed: ["Movie 4"],
-        adventurous: ["Movie 5"],
-        thoughtful: ["Movie 6"]
+        happy: ["The Lego Movie", 
+            "Singin' in the Rain",
+            "Mamma Mia!",
+            "School of Rock",
+            "The Princess Bride",
+            "Ferris Bueller's Day Off",
+            "Crazy Rich Asians",
+            "Shrek",
+            "Madagascar",
+            "The Truman Show"],
+        sad: ["The Green Mile",
+            "Schindler's List",
+            "Marley & Me",
+            "The Fault in Our Stars",
+            "A Walk to Remember",
+            "Hachi: A Dog's Tale",
+            "Bridge to Terabithia",
+            "Million Dollar Baby",
+            "Grave of the Fireflies",
+            "Me Before You"],
+        excited: ["Mad Max: Fury Road",
+            "Avengers: Endgame",
+            "Inception",
+            "Jurassic Park",
+            "The Dark Knight",
+            "Spider-Man: No Way Home",
+            "Guardians of the Galaxy",
+            "The Matrix",
+            "Top Gun",
+            "John Wick"],
+        relaxed: ["Finding Nemo",
+            "Chef",
+            "The Secret Life of Walter Mitty",
+            "Forrest Gump",
+            "About Time",
+            "Before Sunrise",
+            "Spirited Away",
+            "Groundhog Day",
+            "50 First Dates",
+            "21 Jump Street"],
+        adventurous: ["Raiders of the Lost Ark",
+            "Back to the Future",
+            "Star Wars: Episode IV - A New Hope",
+            "The Mummy (1999)",
+            "Jumanji: Welcome to the Jungle",
+            "The Revenant",
+            "Life of Pi",
+            "The Lord of the Rings: The Fellowship of the Ring",
+            "Pirates of the Caribbean: The Curse of the Black Pearl",
+            "The Goonies"],
+        thoughtful: ["Interstellar",
+            "The Pursuit of Happyness",
+            "Good Will Hunting",
+            "A Beautiful Mind",
+            "The Imitation Game",
+            "Dead Poets Society",
+            "The Shawshank Redemption",
+            "The Theory of Everything",
+            "Life Is Beautiful",
+            "Manchester by the Sea"]
     };
     
     moodSelect.addEventListener("change", () => {
@@ -23,6 +77,7 @@ window.onload = function() {
     console.log("moodSelect.value 2:" + moodSelect.value);
 
     surpriseBtn.addEventListener("click", () => {
+        pickMovie(moodSelect.value);
         console.log("surpriseBtn clicked");
     });
 
@@ -37,6 +92,19 @@ window.onload = function() {
 
     function pickMovie(mood)
     {
+        if(!mood || !movies[mood])
+        {
+            return;
+        }
+
+        var movieList = movies[mood];
+        // console.log("movieList.length: " + movieList.length);
+        var randomMovie = movieList[Math.floor(Math.random() * movieList.length)];
         console.log("function pickMovie, mood selected: " + mood);
+        console.log("randomMovie: " + randomMovie);
+
+        movieDisplay.innerText = "Your movie suggestion: " + randomMovie;
+        anotherMovie.style.display = "block";
+        resetBtn.style.display = "block";
     }
 }
